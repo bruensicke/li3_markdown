fdfa3a90800945d3e0729c31657f18aef124e4e4
Tobias Sandelius <tobias.sandelius@gmail.com>
2011-05-12 11:34:03 +0200

    updated readme

--- extensions/helper/Markdown.php
+++ extensions/helper/Markdown.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * li3_markdown: lithium port to PHP Markdown for parsing markdown.
+ * li3_markdown: lithium port to PHP Markdown for parsing markdown markup into HTML.
  *
  * @copyright     Copyright 2011, Tobias Sandelius (http://sandelius.org)
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
